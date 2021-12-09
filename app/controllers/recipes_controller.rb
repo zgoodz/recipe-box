46219da9c6f3c2e733ea82479b850f7d323a6d12
Zachary Goodman <znote89@gmail.com>
2021-12-09 14:15:06 -0500

    Added CRUD to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,35 @@
 class RecipesController < ApplicationController
+
+    def index
+        render json: Recipe.all, status: :ok
+    end
+
+    def show
+        recipe = Recipe.find_by(id: params[:id])
+        render json: recipe, status: :ok
+    end
+
+     def create
+        recipe = Recipe.create!(recipe_params)
+        render json: Recipe.order(:trainer_id), status: :created
+    end
+
+    def update
+        recipe = Recipe.find_by(id: params[:id])
+        recipe.update!(recipe_params)
+        render json: recipe, status: :accepted
+    end
+
+    def destroy
+        recipe = Recipe.find_by(id: params[:id])
+        recipe.destroy
+        head :no_content, status: :ok
+    end
+
+    private
+
+    def recipe_params
+        params.permit(:name, :ingredients, :directions, :user_id)
+    end
+
 end
