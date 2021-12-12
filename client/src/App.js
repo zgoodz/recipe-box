3651c32045b59409e37d4bc7be7172713e6781dd
Zachary Goodman <znote89@gmail.com>
2021-12-12 16:38:58 -0500

    Start Wireframe

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import Home from './Components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar">
+        <div className="logo">RB</div>
+        <div className="menu">
+          <ul className="nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Find Recipe</a></li>
+            <li><a href="/">What 2 Eat?</a></li>
+            <li><a href="/">About</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div className="App">
+        <Home/>
+      </div>
     </div>
   );
 }
