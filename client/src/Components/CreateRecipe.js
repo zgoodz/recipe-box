ee8fbd0f8184f306933a862f1b2d0b4abee2c36a
Zachary Goodman <znote89@gmail.com>
2021-12-30 11:55:30 -0500

    added gameplan

--- client/src/Components/CreateRecipe.js
+++ client/src/Components/CreateRecipe.js
@@ -1,4 +1,14 @@
 export default function CreateRecipe() {
+
+// needs two list items:
+// 1) ingredients -
+//  on one line:
+//  amount, unit of measurement, ingredient
+// 2) instructions
+//  one line text
+// starts with one line each
+// both have plus button to add more lines
+
     return(
         <div>
             <h1>Create your Recipe</h1>
