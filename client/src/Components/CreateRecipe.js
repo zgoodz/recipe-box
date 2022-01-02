a43cbb882973bf3b2a62fa0abefff8b42dcbaf44
Zachary Goodman <znote89@gmail.com>
2022-01-02 14:30:33 -0500

    added extra line feature

--- client/src/Components/CreateRecipe.js
+++ client/src/Components/CreateRecipe.js
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 export default function CreateRecipe() {
+    const [ingredientLength, setIngredientLength] = useState(1)
+    const [instructionLength, setInstructionLength] = useState(1)
 
 // needs two list items:
 // 1) ingredients -
@@ -9,9 +13,28 @@ export default function CreateRecipe() {
 // starts with one line each
 // both have plus button to add more lines
 
+function handleAddIngredient() {
+    setIngredientLength(ingredientLength + 1)
+}
+
+function handleAddInstruction() {
+    setInstructionLength(instructionLength + 1)
+}
     return(
         <div>
             <h1>Create your Recipe</h1>
+            <ul>
+                {ingredientLength.map((item) => {
+                    <li>Test</li>
+                })}
+            </ul>
+            <button onClick={handleAddIngredient}>add Ingredient</button>
+            <ul>
+                {instructionLength.map((item) => {
+                    <li>Test</li>
+                })}
+            </ul>
+            <button onClick={handleAddInstruction}>add Instruction</button>
         </div>
     )
 }
