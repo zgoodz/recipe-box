66e38411f9a692a298e2d68fc4770b1b0ee9eaba
Zachary Goodman <znote89@gmail.com>
2022-01-03 23:47:08 -0500

    reconfigured state to objects

--- client/src/Components/CreateRecipe.js
+++ client/src/Components/CreateRecipe.js
@@ -1,40 +1,41 @@
 import { useState } from 'react'
 
 export default function CreateRecipe() {
-    const [ingredientLength, setIngredientLength] = useState(1)
-    const [instructionLength, setInstructionLength] = useState(1)
-
-// needs two list items:
-// 1) ingredients -
-//  on one line:
-//  amount, unit of measurement, ingredient
-// 2) instructions
-//  one line text
-// starts with one line each
-// both have plus button to add more lines
+    const [ingredients, setIngredients] = useState({})
+    const [instructions, setInstructions] = useState({})
 
 function handleAddIngredient() {
-    setIngredientLength(ingredientLength + 1)
+    setIngredients()
 }
 
 function handleAddInstruction() {
-    setInstructionLength(instructionLength + 1)
+    setInstructions()
 }
+
+function handleSave() {
+
+}
+
     return(
         <div>
             <h1>Create your Recipe</h1>
-            <ul>
-                {ingredientLength.map((item) => {
-                    <li>Test</li>
-                })}
-            </ul>
-            <button onClick={handleAddIngredient}>add Ingredient</button>
-            <ul>
-                {instructionLength.map((item) => {
-                    <li>Test</li>
-                })}
-            </ul>
-            <button onClick={handleAddInstruction}>add Instruction</button>
+            <div className="ingredients">
+                <ul>
+                    {ingredients.map((item) => {
+                        <li>Test</li>
+                    })}
+                </ul>
+                <button onClick={handleAddIngredient}>add Ingredient</button>
+            </div>
+            <div className="instructions">
+                <ul>
+                    {instructions.map((item) => {
+                        <li>Test</li>
+                    })}
+                </ul>
+                <button onClick={handleAddInstruction}>add Instruction</button>
+            </div>
+            <button onClick={handleSave}>Save</button>
         </div>
     )
 }
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,7 +1,10 @@
+import CreateRecipe from "./CreateRecipe.js"
+
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <CreateRecipe/>
         </div>
     )
 }
